f2b2b219f32d1ecee62c664e8858018b7eb968a6
NONAME-lf <mekshakov.art@gmail.com>
2025-10-12 01:02:19 +0300

    Updates

--- assets/CategoryLabel-2SVeUYEl.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DyytaR27.js";function s(a){return e.jsx("span",{className:"label",children:a.category})}export{s as C};
---
+++ assets/PageSlogan-CinBz13S.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-sZzND8N5.js";function e(a){return n.jsx("div",{className:`img-wrap slogan ${a.className||""}`,dangerouslySetInnerHTML:{__html:a.svgContent}})}export{e as P};
